9cdd1177cc5588326057271af575d0c32d183835
pablo <pabloluceroschneider@gmail.com>
2024-01-14 04:51:30 -0300

    Modify services as singletones

--- src/services/api/ApiService.ts
+++ src/services/api/ApiService.ts
@@ -1,13 +1,25 @@
 import axios from 'axios';
 import { FetchAlbumsParams } from '@/types';
 
-const HttpClient = axios.create({
-  baseURL: '/api',
-  timeout: 3000,
-});
+class ApiServiceSingleton {
+  private static instance: any;
 
-export class ApiService {
-  static async fetchAlbums({
+  private domain = '/api';
+
+  private restclient = axios.create({
+    baseURL: this.domain,
+    timeout: 3000,
+  });
+
+  constructor() {
+    if (typeof ApiServiceSingleton.instance === 'object') {
+      return ApiServiceSingleton.instance;
+    }
+    ApiServiceSingleton.instance = this;
+    return this;
+  }
+
+  public async fetchAlbums({
     q,
     year,
     limit,
@@ -16,7 +28,7 @@ export class ApiService {
   }: FetchAlbumsParams) {
     try {
       const params = { q, year, type, offset, limit };
-      const { data } = await HttpClient.get('/spotify/search', {
+      const { data } = await this.restclient.get('/spotify/search', {
         params,
       });
       return data;
@@ -25,12 +37,13 @@ export class ApiService {
     }
   }
 
-  static async fetchAlbumById(id: string) {
+  public async fetchAlbumById(id: string) {
     try {
-      const { data } = await HttpClient.get(`/v1/albums/${id}`);
+      const { data } = await this.restclient.get(`/v1/albums/${id}`);
       return data;
     } catch (error) {
       throw error;
     }
   }
 }
+export const ApiService = new ApiServiceSingleton();
